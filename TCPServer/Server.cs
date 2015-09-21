4331be2dab2aa0c9333b47cd2ec435e7791d9ea4
mamoorlook <mamoorlook@gmail.com>
2015-09-21 10:43:33 +0200

    First commit

---
+++ TCPServer/Server.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPServer
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:8080/");
+            listener.Start();
+
+            HttpListenerContext context = listener.GetContext();
+
+            byte[] byteArr = Encoding.ASCII.GetBytes("Hello World!");
+            context.Response.OutputStream.Write(byteArr, 0, byteArr.Length);
+
+            context.Response.Close();
+
+            listener.Stop();
+        }
+    }
+}
